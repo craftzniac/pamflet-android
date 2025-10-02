bf9c798efbde7d81324507b9c933dac0e2bee6ce
MikeyOnyedika <michaelvik520@gmail.com>
2025-10-02 09:59:37 +0100

    upgrade to pamflet-dsl v0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 }
 
 dependencies {
-    implementation("io.github.craftzniac:pamflet-dsl:0.1.2")
+    implementation("io.github.craftzniac:pamflet-dsl:0.1.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
